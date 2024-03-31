dea33d7a2da87331ba0ddc090942fa961881cfde
Leedsgarden <lkw1411026824@outlook.com>
2024-03-31 09:18:50 +0800

    Add netconfig.sh

---
+++ Test/netconfig.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+INTERFACE="enp4s0"
+
+echo "配置电脑ip"
+sudo ifconfig $INTERFACE 192.168.0.3 netmask 255.255.255.0
+
+echo "当前电脑ip"
+ifconfig $INTERFACE
+
+echo "配置默认网关"
+#sudo route add default gw 192.168.0.1
+
+echo "当前默认网关"
+sudo route -n
+
+echo "绑定开发板ip地址"
+sudo arp -s 192.168.0.2 00:0a:35:01:fe:c0
+
+echo "当前开发板ip地址"
+sudo arp -e
